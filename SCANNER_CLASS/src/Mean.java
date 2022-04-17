808d2f129c046751b11f6bdcfa6e00a82a6beb36
ztabbu <ztabassum9964@gmail.com>
2022-04-18 00:14:07 +0530

    first commit

---
+++ SCANNER_CLASS/src/Mean.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Mean {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int sum=0,count=0;
+		while(sc.hasNextInt())
+		{
+			int num=sc.nextInt();
+			sum+=num;
+			count++;
+		}
+		int mean=sum/count;
+		System.out.println("Mean:" + mean);
+	}
+
+}
